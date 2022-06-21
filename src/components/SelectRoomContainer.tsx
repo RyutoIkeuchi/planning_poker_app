5bed6834c630e2611c34c057f545944483c5455f
ryuto <ryu.i7133@gmail.com>
2022-06-21 23:59:12 +0900

    ポーカーテーブルを作って、usernameとroomIdを登録できるようにする

--- src/components/SelectRoomContainer.tsx
+++ src/components/SelectRoomContainer.tsx
@@ -1,10 +1,10 @@
-import Link from 'next/link';
 import { useEffect, useState } from 'react';
 import { useRouter } from 'next/router';
 
 export const SelectRoomContainer = () => {
 	const [roomId, setRoomId] = useState();
-	const [roomIdToLocalStorage, setRoomIdToLocalStorage] = useState();
+	const [userName, setUserName] = useState('');
+	const [pokersToLocalStorage, setPokersToLocalStorage] = useState([]);
 	const router = useRouter();
 
 	const createRoomId = () => {
@@ -12,15 +12,18 @@ export const SelectRoomContainer = () => {
 		while (randomId.toString().length != 6) {
 			randomId = Math.floor(Math.random() * 1000000);
 		}
-		console.log(randomId);
-		localStorage.setItem('ROOM_ID', randomId.toString());
-		setRoomIdToLocalStorage(randomId);
+
+		const newPokerRoom = JSON.stringify([
+			...pokersToLocalStorage,
+			{ room_id: randomId, users: [userName] },
+		]);
+
+		localStorage.setItem('PLANNING_POKER', newPokerRoom);
+		router.push('/poker_room');
 	};
 
 	const enterTheRoom = () => {
-		console.log(roomId);
-		console.log(roomIdToLocalStorage);
-		if (roomId === roomIdToLocalStorage) {
+		if (pokersToLocalStorage.some((poker) => poker.room_id == roomId)) {
 			router.push('/poker_room');
 		}
 	};
@@ -29,15 +32,22 @@ export const SelectRoomContainer = () => {
 		setRoomId(e.target.value);
 	};
 
+	const changeUserName = (e) => {
+		setUserName(e.target.value);
+	};
+
 	useEffect(() => {
-		const roomId = localStorage.getItem('ROOM_ID');
-		setRoomIdToLocalStorage(roomId);
+		const json = localStorage.getItem('PLANNING_POKER');
+		if (json != null) {
+			setPokersToLocalStorage(JSON.parse(json));
+		}
+		console.log(localStorage.getItem('PLANNING_POKER'));
 	}, []);
 
 	return (
 		<div>
-			<p>{roomIdToLocalStorage}</p>
 			<button onClick={createRoomId}>部屋を作る</button>
+			<input type="text" onChange={changeUserName} className="border" />
 			<input type="text" onChange={changeRoomId} className="border" />
 			<button onClick={enterTheRoom} className="text-blue-400">
 				部屋に入る
