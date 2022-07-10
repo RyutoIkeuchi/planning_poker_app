5a485bdfe3919835aaf8d3f6658632c1a06b3ebf
ryuto <ryu.i7133@gmail.com>
2022-07-10 15:52:56 +0900

    ポーカールーム画面のUIを整える

--- src/pages/poker-room/[id].tsx
+++ src/pages/poker-room/[id].tsx
@@ -63,12 +63,38 @@ const PokerRoom = () => {
 
 	return (
 		<div>
-			<h1>プランニングポーカー{router.query.id}</h1>
-			<button onClick={handleLeaveTheRoom}>部屋を退出する</button>
-			<ul>
-				{users.map((user) => (
-					<li key={user.id}>{user.name}</li>
-				))}
+			<div className="my-6 flex justify-between items-center">
+				<h3 className="">
+					Room ID : <span className="font-bold text-xl">{router.query.id}</span>
+				</h3>
+				<button
+					onClick={handleLeaveTheRoom}
+					className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
+				>
+					部屋を退出する
+				</button>
+			</div>
+			<ul className="flex justify-start">
+				{users.map((user) => {
+					if (roomDataToLocalStorage?.name == user.name) {
+						return (
+							<li key={user.id} className="text-red-600">
+								<div className="w-20 h-28 border border-blue-600 shadow-lg flex justify-center items-center mb-4 mr-4">
+									<p className="text-3xl">?</p>
+								</div>
+								<p className="text-center">{user.name}</p>
+							</li>
+						);
+					}
+					return (
+						<li key={user.id}>
+							<div className="w-20 h-28 border border-blue-600 shadow-lg flex justify-center items-center mb-4 mr-4">
+								<p className="text-3xl">?</p>
+							</div>
+							<p className="text-center">{user.name}</p>
+						</li>
+					);
+				})}
 			</ul>
 		</div>
 	);
