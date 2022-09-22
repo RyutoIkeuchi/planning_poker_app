102c844b5ec0d209da60874894748ee84128348c
ryuto <ryu.i7133@gmail.com>
2022-09-22 15:15:56 +0900

    選択済のユーザーは文言を「済」にする

--- src/pages/poker-room/[id].tsx
+++ src/pages/poker-room/[id].tsx
@@ -302,7 +302,11 @@ const PokerRoom = () => {
 									<li key={index} className="text-red-600 w-28 h-40 mr-10">
 										<div className="w-full h-full border border-blue-600 shadow-lg flex justify-center items-center mb-4">
 											<p className="text-3xl">
-												{isSelectNumberResult ? user.selectCard : '?'}
+												{isSelectNumberResult
+													? user.selectCard
+													: user.isSelected
+													? '済'
+													: '未'}
 											</p>
 										</div>
 										<div className="flex justify-center items-center">
@@ -314,7 +318,11 @@ const PokerRoom = () => {
 									<li key={index} className="w-28 h-40 mr-10">
 										<div className="w-full h-full border border-blue-600 shadow-lg flex justify-center items-center mb-4">
 											<p className="text-3xl">
-												{isSelectNumberResult ? user.selectCard : '?'}
+												{isSelectNumberResult
+													? user.selectCard
+													: user.isSelected
+													? '済'
+													: '未'}
 											</p>
 										</div>
 										<div className="flex justify-center items-center">
