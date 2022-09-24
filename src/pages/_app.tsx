16354a5ab26700115c097478f14ec1938dce319a
ryuto <ryu.i7133@gmail.com>
2022-09-24 16:51:29 +0900

    fontawesomeアイコンがcssより先に読み込まれないようにする

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,10 @@
 import 'src/styles/globals.css';
 import type { AppProps } from 'next/app';
 import { Layout } from 'src/components/Layout';
+import '@fortawesome/fontawesome-svg-core/styles.css';
+import { config } from '@fortawesome/fontawesome-svg-core';
+
+config.autoAddCss = false;
 
 function MyApp({ Component, pageProps }: AppProps) {
 	return (
