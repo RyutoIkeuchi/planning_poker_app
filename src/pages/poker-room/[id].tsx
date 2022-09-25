14d9f5f4a4f5968c0d32a7941a570d03ff1cf6be
ryuto <ryu.i7133@gmail.com>
2022-09-25 15:05:29 +0900

    ♻️ ユーザーカード一覧の条件分岐をより簡潔に記載

--- src/pages/poker-room/[id].tsx
+++ src/pages/poker-room/[id].tsx
@@ -344,41 +344,27 @@ const PokerRoom = () => {
 				<ul className="flex justify-start">
 					{myRoomUsers.map((user, index) => {
 						return (
-							<>
-								{user.userName === roomDataToLocalStorage?.userName ? (
-									<li key={index} className="text-red-600 w-28 h-40 mr-10">
-										<div className="w-full h-full border border-blue-600 shadow-lg flex justify-center items-center mb-4">
-											<p className="text-3xl">
-												{isSelectNumberResult
-													? user.selectCard
-													: user.isSelected
-													? '済'
-													: '未'}
-											</p>
-										</div>
-										<div className="flex justify-center items-center">
-											{user.hostUser && <FontAwesomeIcon icon={faUserTie} />}
-											<p className="text-center ml-1">{user.userName}</p>
-										</div>
-									</li>
-								) : (
-									<li key={index} className="w-28 h-40 mr-10">
-										<div className="w-full h-full border border-blue-600 shadow-lg flex justify-center items-center mb-4">
-											<p className="text-3xl">
-												{isSelectNumberResult
-													? user.selectCard
-													: user.isSelected
-													? '済'
-													: '未'}
-											</p>
-										</div>
-										<div className="flex justify-center items-center">
-											{user.hostUser && <FontAwesomeIcon icon={faUserTie} />}
-											<p className="text-center ml-1">{user.userName}</p>
-										</div>
-									</li>
-								)}
-							</>
+							<li
+								key={index}
+								className={`w-28 h-40 mr-10 ${
+									user.userName === roomDataToLocalStorage?.userName &&
+									'text-red-600'
+								}`}
+							>
+								<div className="w-full h-full border border-blue-600 shadow-lg flex justify-center items-center mb-4">
+									<p className="text-3xl">
+										{isSelectNumberResult
+											? user.selectCard
+											: user.isSelected
+											? '済'
+											: '未'}
+									</p>
+								</div>
+								<div className="flex justify-center items-center">
+									{user.hostUser && <FontAwesomeIcon icon={faUserTie} />}
+									<p className="text-center ml-1">{user.userName}</p>
+								</div>
+							</li>
 						);
 					})}
 				</ul>
