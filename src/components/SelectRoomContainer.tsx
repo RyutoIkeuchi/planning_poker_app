81e7f59ec2dc32f0a923e78e11881bd896c51204
ryuto <ryu.i7133@gmail.com>
2022-10-01 13:56:43 +0900

    初期画面内で同じ処理をしてる箇所をコンポーネントで切り分ける

--- src/components/SelectRoomContainer.tsx
+++ src/components/SelectRoomContainer.tsx
@@ -3,37 +3,45 @@ import EnterRoomIcon from 'public/images/EnterRoom_Flatline.svg';
 import CreateRoomIcon from 'public/images/CreateRoom_Flatline.svg';
 import { Footer } from 'src/components/Footer';
 
+type RoomContentType = {
+	icon: React.ReactElement;
+	href: string;
+	linkLabel: string;
+};
+
 export const SelectRoomContainer = () => {
 	return (
 		<>
 			<div className="flex justify-center items-center min-h-screen">
 				<div className="flex justify-between w-full">
-					<div>
-						<div className="mb-4">
-							<CreateRoomIcon />
-						</div>
-						<Link href="/create-room">
-							<a className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded w-full mb-4 text-center block">
-								部屋を作る
-							</a>
-						</Link>
-					</div>
+					<RoomContent
+						icon={<CreateRoomIcon />}
+						href="/create-room"
+						linkLabel="部屋を作る"
+					/>
 					<div className="h-auto border border-blue-700"></div>
-					<div>
-						<div className="mb-4">
-							<EnterRoomIcon />
-						</div>
-						<div className="w-full">
-							<Link href="/enter-room">
-								<a className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded w-full mb-4 text-center block">
-									部屋に入る
-								</a>
-							</Link>
-						</div>
-					</div>
+					<RoomContent
+						icon={<EnterRoomIcon />}
+						href="/enter-room"
+						linkLabel="部屋に入る"
+					/>
 				</div>
 			</div>
 			<Footer />
 		</>
+	);
+};
+
+const RoomContent = (props: RoomContentType) => {
+	const { icon, href, linkLabel } = props;
+	return (
+		<div>
+			<div className="mb-4">{icon}</div>
+			<Link href={href}>
+				<a className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded w-full mb-4 text-center block">
+					{linkLabel}
+				</a>
+			</Link>
+		</div>
 	);
 };
