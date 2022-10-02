c9160329838310a177b9cef223997cd164bd4f24
ryuto <ryu.i7133@gmail.com>
2022-10-02 15:14:27 +0900

    ブラウザバックをさせないようにする

--- src/pages/poker-room/[id].tsx
+++ src/pages/poker-room/[id].tsx
@@ -326,10 +326,18 @@ const PokerRoom = () => {
 		event.returnValue = 'ポーカールーム画面から離れます';
 	}, []);
 
+	const handlePopState = useCallback((e) => {
+		alert('ブラウザバックを使わないでください。');
+		history.go(1);
+	}, []);
+
 	useEffect(() => {
+		history.pushState(null, null, null);
+		window.addEventListener('popstate', handlePopState);
 		window.addEventListener('beforeunload', handleBeforeUnload);
 		return () => {
 			window.removeEventListener('beforeunload', handleBeforeUnload);
+			window.addEventListener('popstate', handlePopState);
 		};
 	}, []);
 
