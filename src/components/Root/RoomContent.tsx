710c7fbca36d777a37fded5afc4e3ebb02d5e7c1
ryuto <ryu.i7133@gmail.com>
2022-10-13 18:08:52 +0900

    部屋を作るときは遷移する前に確認モーダルを表示させる

--- src/components/Root/RoomContent.tsx
+++ src/components/Root/RoomContent.tsx
@@ -1,4 +1,5 @@
 import Link from "next/link";
+import { useRouter } from "next/router";
 import React from "react";
 
 type RoomContentType = {
@@ -9,14 +10,31 @@ type RoomContentType = {
 
 export const RoomContent = (props: RoomContentType) => {
   const { href, icon, linkLabel } = props;
+  const router = useRouter();
+  const confirmNavigationToCreateRoom = (href: string) => {
+    const isConfirmed = confirm("部屋を作成します。よろしいですか？");
+    if (!isConfirmed) {
+      return;
+    }
+    router.push(href);
+  };
   return (
     <div>
       <div className="mb-4">{icon}</div>
-      <Link href={href}>
-        <a className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded w-full mb-4 text-center block">
+      {linkLabel === "部屋を作る" ? (
+        <button
+          className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded w-full mb-4 text-center block"
+          onClick={() => confirmNavigationToCreateRoom(href)}
+        >
           {linkLabel}
-        </a>
-      </Link>
+        </button>
+      ) : (
+        <Link href={href}>
+          <a className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded w-full mb-4 text-center block">
+            {linkLabel}
+          </a>
+        </Link>
+      )}
     </div>
   );
 };
