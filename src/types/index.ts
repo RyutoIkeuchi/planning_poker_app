fcc37756c87ee2874d776f0f4878761f8112c6b1
ryuto <ryu.i7133@gmail.com>
2022-10-17 23:03:53 +0900

    型定義方法を変更

--- src/types/index.ts
+++ src/types/index.ts
@@ -10,10 +10,8 @@ export type UserType = ToLocalStorageUserType & {
   isSelected: boolean;
 };
 
-export type ResSelectedNumberCardType = {
-  roomId: string;
-  selectedNumberCard: string;
-  userName: string;
-};
+export type ResSelectedNumberCardType = Required<
+  Pick<ToLocalStorageUserType, "roomId" | "selectedNumberCard" | "userName">
+>;
 
 export type PokerStatusType = "result" | "reset" | "default";
