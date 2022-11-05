dd2d84f9b7b575f545862b292d91dc03e831f264
ryuto <ryu.i7133@gmail.com>
2022-11-05 17:39:13 +0900

    議題入力エリアコンポーネントのリファクタリング・mutateで更新を速くする

--- src/components/PokerRoom/InputAgendaTitle.tsx
+++ src/components/PokerRoom/InputAgendaTitle.tsx
@@ -9,60 +9,72 @@ type Props = {
 
 export const InputAgendaTitle = (props: Props) => {
   const { roomId } = props;
-  const { roomData } = usePokerRoom(roomId);
-  const [agendaTitle, setAgendaTitle] = useState<string>(roomData.agendaTitle);
+  const { mutate, roomData } = usePokerRoom(roomId);
+  const [inputAgendaTitle, setInputAgendaTitle] = useState<string>(roomData.agendaTitle);
 
-  const memoIsSubmitAgendaTitleDisabled = useMemo(() => {
+  const isSubmitButtonDisabled = useMemo(() => {
+    if ((roomData && roomData.agendaTitle !== "") || inputAgendaTitle === "") {
+      return true;
+    }
+    return false;
+  }, [roomData, inputAgendaTitle]);
+
+  const canChangeInputAgendaTitle = useMemo(() => {
     if (roomData && roomData.agendaTitle !== "") {
+      return false;
+    }
+    return true;
+  }, [roomData]);
+
+  const isCancelButtonDisabled = useMemo(() => {
+    if (roomData && roomData.agendaTitle === "") {
       return true;
     }
     return false;
   }, [roomData]);
 
-  const memoCanChangeAgendaTitle = useMemo(() => {
-    if (roomData && roomData.agendaTitle !== "") {
-      return false;
-    }
-    return true;
-  }, [roomData]);
-
-  const memoIsCancelAgendaTitleDisabled = useMemo(() => {
-    if ((roomData && roomData.agendaTitle !== "") || agendaTitle !== "") {
-      return false;
-    }
-    return true;
-  }, [roomData]);
-
   const handleChangeAgendaTitle = useCallback((e: ChangeEvent<HTMLInputElement>) => {
-    const inputAgendaTitle = e.target.value;
-    setAgendaTitle(inputAgendaTitle);
+    const inputText = e.target.value;
+    setInputAgendaTitle(inputText);
   }, []);
 
   const handleSubmitAgendaTitle = useCallback(async () => {
     const data = {
-      agenda_title: agendaTitle,
+      agenda_title: inputAgendaTitle,
     };
     await api.put(`/pokers/${roomId}`, data);
-  }, [agendaTitle, roomId]);
+    await mutate((data) => {
+      return {
+        ...data,
+        agendaTitle: inputAgendaTitle,
+      };
+    });
+  }, [inputAgendaTitle, roomId, mutate]);
 
   const handleCancelAgendaTitle = useCallback(async () => {
-    setAgendaTitle("");
+    setInputAgendaTitle("");
     const data = {
       agenda_title: "",
     };
     await api.put(`/pokers/${roomId}`, data);
     await api.put(`/pokers/${roomId}/users`);
-  }, [roomId]);
+    await mutate((data) => {
+      return {
+        ...data,
+        agendaTitle: "",
+      };
+    });
+  }, [roomId, mutate]);
 
   return (
     <div className="flex justify-start items-center w-2/3">
       <div className="mr-4 w-1/2">
         <input
           type="text"
-          value={agendaTitle}
+          value={inputAgendaTitle}
           className="border p-2 w-full rounded"
           onChange={handleChangeAgendaTitle}
-          disabled={!memoCanChangeAgendaTitle}
+          disabled={!canChangeInputAgendaTitle}
           placeholder="議題を入力"
         />
       </div>
@@ -71,7 +83,7 @@ export const InputAgendaTitle = (props: Props) => {
           buttonColor="bg-green-500"
           hoverButtonColor="hover:bg-green-700"
           handleClickMethod={handleSubmitAgendaTitle}
-          disabled={memoIsSubmitAgendaTitleDisabled}
+          disabled={isSubmitButtonDisabled}
         >
           決定
         </PrimaryButton>
@@ -80,7 +92,7 @@ export const InputAgendaTitle = (props: Props) => {
         buttonColor="bg-orange-500"
         hoverButtonColor="hover:bg-orange-700"
         handleClickMethod={handleCancelAgendaTitle}
-        disabled={memoIsCancelAgendaTitleDisabled}
+        disabled={isCancelButtonDisabled}
       >
         取り消し
       </PrimaryButton>
