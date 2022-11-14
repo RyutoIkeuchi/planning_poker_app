49aa6198d8ce57c7f449c334eacc5b6b9385dc36
ryuto <ryu.i7133@gmail.com>
2022-11-14 22:22:04 +0900

    「部屋を作る」ボタンを押したときに、confirmが表示されるかのtestコードを作成

--- src/__tests__/SelectRoomContainer.test.tsx
+++ src/__tests__/SelectRoomContainer.test.tsx
@@ -1,6 +1,7 @@
-import { render, screen } from "@testing-library/react";
+import { fireEvent, render, screen } from "@testing-library/react";
 import "@testing-library/jest-dom";
 import { SelectRoomContainer } from "src/components/Root/SelectRoomContainer";
+import userEvent from "@testing-library/user-event";
 
 describe("Test SelectRoomContainer Component", () => {
   test("Navigator form with 2 button", async () => {
@@ -13,5 +14,13 @@ describe("Test SelectRoomContainer Component", () => {
     render(<SelectRoomContainer />);
     expect(screen.getByText("部屋を作る")).toBeInTheDocument();
     expect(screen.getByText("部屋に入る")).toBeInTheDocument();
+  });
+
+  it("Should call create-room button", async () => {
+    render(<SelectRoomContainer />);
+    const spyWindowConfirm = jest.spyOn(window, "confirm");
+    spyWindowConfirm.mockImplementation(jest.fn());
+    await userEvent.click(screen.getByText("部屋を作る"));
+    expect(spyWindowConfirm).toHaveBeenCalledTimes(1);
   });
 });
