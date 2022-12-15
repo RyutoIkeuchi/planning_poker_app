158af00b4f2c301b5fdc2bae8b9eb69e49acd243
ryuto <ryu.i7133@gmail.com>
2022-12-16 00:00:13 +0900

    ボタンを押した時に、指定したpathに遷移しているかテスト

--- src/__tests__/EnterRoom.test.tsx
+++ src/__tests__/EnterRoom.test.tsx
@@ -1,9 +1,18 @@
-import { render, screen } from "@testing-library/react";
+import { fireEvent, render, screen, waitFor } from "@testing-library/react";
 import "@testing-library/jest-dom";
 import EnterRoom from "src/pages/enter-room";
 import userEvent from "@testing-library/user-event";
+import singletonRouter, { useRouter } from "next/router";
+import mockRouter from "next-router-mock";
+
+jest.mock("next/router", () => require("next-router-mock"));
+jest.mock("next/dist/client/router", () => require("next-router-mock"));
 
 describe("<EnterRoom />", () => {
+  beforeEach(() => {
+    mockRouter.setCurrentUrl("/");
+  });
+
   test("画面内にボタンが一つあること", async () => {
     render(<EnterRoom />);
     const buttonList = await screen.findAllByRole("button");
@@ -20,5 +29,17 @@ describe("<EnterRoom />", () => {
     const inputRoomId: HTMLInputElement = await screen.findByTestId("input-roomid");
     await userEvent.type(inputRoomId, "1234567");
     expect(inputRoomId.value).toBe("1234567");
+  });
+
+  // TODO: 要改善
+  test("ログインしていないユーザー", async () => {
+    render(<EnterRoom />);
+    const enterRoomButton = screen.getByText("部屋に入る");
+    const inputRoomId: HTMLInputElement = await screen.findByTestId("input-roomid");
+    await userEvent.type(inputRoomId, "1234567");
+    singletonRouter.push("/poker-room/1234567");
+    expect(singletonRouter).toMatchObject({
+      pathname: "/poker-room/1234567",
+    });
   });
 });
